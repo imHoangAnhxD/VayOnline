7ac6cb82fb637412edb4dc0d1b1ea0e83e9bb162
Nguyen Hoang Anh <hoangkye312@gmail.com>
2023-07-14 09:51:50 +0700

    https://trello.com/c/TlGEV6n4 https://trello.com/c/knq3kLw1 https://trello.com/c/ij1Nv3Sm

---
+++ nghenghiep.sql
@@ -0,0 +1,9 @@
+CREATE TABLE nghenghiep (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  value VARCHAR(255) NOT NULL
+);
+INSERT INTO nghenghiep (value) VALUES ('Bác sĩ');
+INSERT INTO nghenghiep (value) VALUES ('Y tá');
+INSERT INTO nghenghiep (value) VALUES ('Diễn viên');
+INSERT INTO nghenghiep (value) VALUES ('Công an');
+INSERT INTO nghenghiep (value) VALUES ('Giáo viên');
