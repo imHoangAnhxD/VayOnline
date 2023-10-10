269a2d66ac90179ceb41015b72c8ed5c81e147d7
Nguyen Hoang Anh <hoangkye312@gmail.com>
2023-10-10 22:53:48 +0700

    update database

---
+++ database/tygia.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tygia (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  mua VARCHAR(255) NOT NULL,
+  ban VARCHAR(255) NOT NULL
+);
+INSERT INTO tygia (name,mua,ban) VALUES ('Đô la Mỹ',"23.400","25.216");
+INSERT INTO tygia  (name,mua,ban) VALUES ('Đồng Euro',"24.172","26.716");
+INSERT INTO tygia  (name,mua,ban) VALUES ('Yên Nhật',"154","170");
