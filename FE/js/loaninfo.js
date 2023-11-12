43089cb2af847fce6a9d68d573f2f26d931bacca
Nguyen Hoang Anh <hoangkye312@gmail.com>
2023-11-12 21:32:47 +0700

    hoanganh

--- FE/js/loaninfo.js
+++ FE/js/loaninfo.js
@@ -1,19 +1,19 @@
 function calculate() {
-    var moneytb=document.getElementById("money-tb");
-    moneytb.style.display='block';
+    var moneytb = document.getElementById("money-tb");
+    moneytb.style.display = 'block';
+
     var loanAmount = parseFloat(document.getElementById('loanAmount').value);
     var loanTerm = parseInt(document.getElementById('loanTerm').value);
-
     var monthlyInterest = 0.03; // Lãi suất hàng tháng (3%)
-    var monthlyPayment = loanAmount / loanTerm;
+    var monthlyPayment = loanAmount * (monthlyInterest / (1 - Math.pow(1 + monthlyInterest, -loanTerm))); // Công thức tính trả nợ hàng tháng
 
     var resultTable = document.getElementById('resultTable');
     resultTable.innerHTML = '';
-    
+
     for (var i = 1; i <= loanTerm; i++) {
-        var interest = loanAmount * monthlyInterest; //tính lãi cho tháng hiện tại bằng cách nhân số tiền còn lại với tỷ lệ lãi hàng tháng.
-        var principal = monthlyPayment - interest; //tính gốc cho tháng hiện tại bằng cách trừ lãi từ khoản trả hàng tháng.
-        loanAmount -= principal; //Dòng này giảm số tiền còn lại của khoản vay đi số tiền gốc đã trả trong tháng hiện tại.
+        var interest = loanAmount * monthlyInterest; // Tính lãi cho tháng hiện tại bằng cách nhân số tiền còn lại với tỷ lệ lãi hàng tháng.
+        var principal = monthlyPayment - interest; // Tính gốc cho tháng hiện tại bằng cách trừ lãi từ khoản trả hàng tháng.
+        loanAmount -= principal; // Giảm số tiền còn lại của khoản vay đi số tiền gốc đã trả trong tháng hiện tại.
 
         var row = document.createElement('tr');
         row.innerHTML = '<td>' + i + '</td>' +
